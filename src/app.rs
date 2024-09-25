d83cda835554cb7909f0f72b3b4f75e3d6c6899a
jeermuce <ernesto_mc.dev@proton.me>
2024-09-24 20:06:06 -0600

    add examples of counter and boolean, grid of images, reset more complete

--- src/app.rs
+++ src/app.rs
@@ -1,17 +1,82 @@
+use leptos::SignalUpdate;
+
 #[leptos::component]
 pub(crate) fn App() -> impl leptos::IntoView {
+    let (truth, set_truthy) = leptos::create_signal(false);
+    let (count, set_count) = leptos::create_signal(0);
     leptos::view! {
         <Header />
-        <main class="flex-1"></main>
-        <footer class=""></footer>
+        <main class="flex flex-col flex-nowrap flex-1 gap-4 p-4 mx-auto bg-green-300">
+            <div class="flex flex-col justify-center items-center">
+                <div class="flex flex-row flex-nowrap gap-3 justify-between items-center">
+                    <h1 class="text-2xl font-black w-fit">sh: {truth}</h1>
+                    <h1 class="text-2xl font-black w-fit">re: {move || truth}</h1>
+                    <h1 class="text-2xl font-black w-fit">nr: {truth()}</h1>
+                </div>
+                <button on:mousedown=move |_| {
+                    set_truthy(!truth());
+                }>swap</button>
+            </div>
+            <div class="flex flex-col justify-center items-center">
+                <div class="flex flex-row flex-nowrap gap-3 justify-between items-center">
+                    <h1 class="text-2xl font-black w-fit">sh: {count}</h1>
+                    <h1 class="text-2xl font-black w-fit">re: {move || count}</h1>
+                    <h1 class="text-2xl font-black w-fit">nr: {count()}</h1>
+                </div>
+                <div class="flex flex-row gap-3">
+                    <button
+                        class="w-16 h-fit"
+                        on:mousedown=move |_| {
+                            set_count.update(|count| *count += 1);
+                        }
+                    >
+                        {"+"}
+                    </button>
+                    <button
+                        class="w-16 h-fit"
+                        on:mousedown=move |_| {
+                            set_count.update(|count| *count -= 1);
+                        }
+                    >
+                        {"-"}
+                    </button>
+                </div>
+            </div>
+
+            <div class="flex flex-row flex-wrap w-fit h-fit"></div>
+            <CardGrid />
+        </main>
+        <Footer />
     }
+}
+
+#[leptos::component]
+pub(crate) fn CardGrid() -> impl leptos::IntoView {
+    leptos::view! {
+        <div class="grid grid-cols-[repeat(auto-fill,minmax(8rem,1fr))] gap-4">
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+
+        </div>
+    }
+}
+
+#[leptos::component]
+pub(crate) fn Card() -> impl leptos::IntoView {
+    leptos::view! { <img src="https://picsum.photos/200/300" alt="image" /> }
 }
 
 #[leptos::component]
 pub(crate) fn Header() -> impl leptos::IntoView {
     leptos::view! {
-        <header class="w-full h-14 flex flex-row items-center justify-center">
-            <nav class="text-white flex flex-row justify-between items-center w-4/5 px-4 h-full  ">
+        <header class="flex flex-row justify-center items-center w-full h-14 bg-black">
+            <nav class="flex flex-row justify-between items-center px-4 w-full max-w-screen-lg h-full text-white">
                 <a href="/">
                     <h1 class="text-2xl">CSR</h1>
                 </a>
@@ -24,35 +89,44 @@ pub(crate) fn Header() -> impl leptos::IntoView {
 }
 
 #[leptos::component]
+pub(crate) fn Footer() -> impl leptos::IntoView {
+    leptos::view! {
+        <footer class="flex flex-row justify-center items-center w-full h-auto bg-white">
+            <Header />
+        </footer>
+    }
+}
+
+#[leptos::component]
 pub(crate) fn Search() -> impl leptos::IntoView {
     leptos::view! {
-        <div class="flex relative flex-row items-center w-2/4 rounded-full">
+        <div class="flex relative flex-row items-center w-2/4 min-h-14 child:min-h-10">
             <input
+
                 type="text"
                 name="Search"
                 id="search"
-                class="rounded-full flex-1 placeholder:text-start px-2"
+                required
+                class="flex-1 px-2 rounded-full appearance-none outline-none placeholder:text-start"
                 placeholder="buscar..."
             />
-            <button class="h-6 w-6 absolute right-0 rounded-full items-center hover:outline justify-center flex">
-                <div class="flex items-center over scale-50">
 
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke-width="1.5"
-                        stroke="currentColor"
-                        class="w-6 h-6"
-                    >
-                        <path
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                            d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"
-                        />
-                    </svg>
+            <button class="flex absolute right-0 justify-center items-center rounded-full search-button aspect-square">
 
-                </div>
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke-width="1.5"
+                    stroke="currentColor"
+                    class="w-8 h-8"
+                >
+                    <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"
+                    />
+                </svg>
             </button>
         </div>
     }
@@ -65,13 +139,17 @@ pub(crate) fn Burger() -> impl leptos::IntoView {
             type="checkbox"
             name="Burger"
             id="burger"
-            class="checked:scale-150 hidden peer/invisible"
+            class="hidden checked:scale-150 peer/invisible"
         />
         <label
             for="burger"
             class="absolute top-0 left-0 w-full h-full -z-50 peer-checked:invisible"
         />
-        <button class="w-6 h-6 rounded-full text-center ">
+        <button
+            class="flex justify-center items-center rounded-full search-button aspect-square"
+            // this button should open an alert saying Ive been clicked
+            onclick="alert('Ive been clicked')"
+        >
             <svg
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
@@ -88,6 +166,6 @@ pub(crate) fn Burger() -> impl leptos::IntoView {
             </svg>
 
         </button>
-        <menu class="relative flex-row items-center justify-center hidden"></menu>
+        <menu class="hidden relative flex-row justify-center items-center"></menu>
     }
 }
